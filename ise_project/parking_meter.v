670e7122a8245d98bdafdeb964b5d52c82564be3
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2022-05-16 16:52:36 -0700

    Project + top level module

---
+++ ise_project/parking_meter.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:48:46 05/16/2022 
+// Design Name: 
+// Module Name:    parking_meter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module parking_meter(
+    input clk,
+    input [7:0] sw,
+    input btns,
+    input btnu,
+    input btnl,
+    input btnd,
+    input btnr,
+    input [7:0] JA,
+    output [7:0] seg,
+    output [3:0] an
+    );
+
+
+endmodule
