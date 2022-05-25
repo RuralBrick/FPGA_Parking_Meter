d1101d994d3a50fedb4afaf633ec7e77be76658d
Jiamin Xu <jiaminxu1019@gmail.com>
2022-05-25 15:18:12 -0700

    decimals

--- ise_project/cost_convert.v
+++ ise_project/cost_convert.v
@@ -24,131 +24,129 @@ module cost_convert(
   }
  end
 
- //based on the location and time, calculate cost in cents accordingly
- if (location == 'b000) begin
-  //12 am to 8 am
-  if (hour >= 'b00000 && hour < 'b01000) begin
-   cost = min_count * 1.45;
-  //8 am to 1 pm
-  end else if (hour >= 'b01000 && hour < 'b01101) begin
-   cost = min_count * 1.67;
-  //1 pm to 6 pm
-  end else if (hour >= 'b01101 && hour < 'b10010) begin
-   cost = min_count * 1.89;
-  //6 pm to 12 am
-  end else if (hour >= 'b10010 && hour < 'b11000) begin
-     cost = min_count * 1.45;
-  end
+   //based on the location and time, calculate cost in cents accordingly
+   if (location == 'b000) begin
+   //12 am to 8 am
+   if (hour >= 'b00000 && hour < 'b01000) begin
+      cost = min_count * 1;
+   //8 am to 1 pm
+   end else if (hour >= 'b01000 && hour < 'b01101) begin
+      cost = min_count * 2;
+   //1 pm to 6 pm
+   end else if (hour >= 'b01101 && hour < 'b10010) begin
+      cost = min_count * 2;
+   //6 pm to 12 am
+   end else if (hour >= 'b10010 && hour < 'b11000) begin
+      cost = min_count * 1;
+   end
 
- end else if (location == 'b001) begin
-  //12 am to 8 am
-  if (hour >= 'b00000 && hour < 'b01000) begin
-   cost = min_count * 1.34;
-  //8 am to 1 pm
-  end else if (hour >= 'b01000 && hour < 'b01101) begin
-   cost = min_count * 1.56;
-  //1 pm to 6 pm
-  end else if (hour >= 'b01101 && hour < 'b10010) begin
-   cost = min_count * 1.78;
-  //6 pm to 12 am
-  end else if (hour >= 'b10010 && hour < 'b11000) begin
-     cost = min_count * 1.56;
-  end
+   end else if (location == 'b001) begin
+   //12 am to 8 am
+   if (hour >= 'b00000 && hour < 'b01000) begin
+      cost = min_count * 1;
+   //8 am to 1 pm
+   end else if (hour >= 'b01000 && hour < 'b01101) begin
+      cost = min_count * 2;
+   //1 pm to 6 pm
+   end else if (hour >= 'b01101 && hour < 'b10010) begin
+      cost = min_count * 2;
+   //6 pm to 12 am
+   end else if (hour >= 'b10010 && hour < 'b11000) begin
+      cost = min_count * 1;
+   end
 
- end else if (location == 'b010) begin
-  //12 am to 8 am
-  if (hour >= 'b00000 && hour < 'b01000) begin
-   cost = min_count * 1.34;
-  //8 am to 1 pm
-  end else if (hour >= 'b01000 && hour < 'b01101) begin
-   cost = min_count * 1.56;
-  //1 pm to 6 pm
-  end else if (hour >= 'b01101 && hour < 'b10010) begin
-   cost = min_count * 1.78;
-  //6 pm to 12 am
-  end else if (hour >= 'b10010 && hour < 'b11000) begin
-     cost = min_count * 1.56;
-  end
+   end else if (location == 'b010) begin
+   //12 am to 8 am
+   if (hour >= 'b00000 && hour < 'b01000) begin
+      cost = min_count * 1;
+   //8 am to 1 pm
+   end else if (hour >= 'b01000 && hour < 'b01101) begin
+      cost = min_count * 2;
+   //1 pm to 6 pm
+   end else if (hour >= 'b01101 && hour < 'b10010) begin
+      cost = min_count * 2;
+   //6 pm to 12 am
+   end else if (hour >= 'b10010 && hour < 'b11000) begin
+      cost = min_count * 1;
+   end
 
- end else if (location == 'b011) begin
-  //12 am to 8 am
-  if (hour >= 'b00000 && hour < 'b01000) begin
-   cost = min_count * 1.34;
-  //8 am to 1 pm
-  end else if (hour >= 'b01000 && hour < 'b01101) begin
-   cost = min_count * 1.56;
-  //1 pm to 6 pm
-  end else if (hour >= 'b01101 && hour < 'b10010) begin
-   cost = min_count * 1.78;
-  //6 pm to 12 am
-  end else if (hour >= 'b10010 && hour < 'b11000) begin
-     cost = min_count * 1.56;
-  end
+   end else if (location == 'b011) begin
+   //12 am to 8 am
+   if (hour >= 'b00000 && hour < 'b01000) begin
+      cost = min_count * 1;
+   //8 am to 1 pm
+   end else if (hour >= 'b01000 && hour < 'b01101) begin
+      cost = min_count * 2;
+   //1 pm to 6 pm
+   end else if (hour >= 'b01101 && hour < 'b10010) begin
+      cost = min_count * 2;
+   //6 pm to 12 am
+   end else if (hour >= 'b10010 && hour < 'b11000) begin
+      cost = min_count * 1;
+   end
 
- end else if (location == 'b100) begin
-  //12 am to 8 am
-  if (hour >= 'b00000 && hour < 'b01000) begin
-   cost = min_count * 1.34;
-  //8 am to 1 pm
-  end else if (hour >= 'b01000 && hour < 'b01101) begin
-   cost = min_count * 1.56;
-  //1 pm to 6 pm
-  end else if (hour >= 'b01101 && hour < 'b10010) begin
-   cost = min_count * 1.78;
-  //6 pm to 12 am
-  end else if (hour >= 'b10010 && hour < 'b11000) begin
-     cost = min_count * 1.56;
-  end
+   end else if (location == 'b100) begin
+   //12 am to 8 am
+   if (hour >= 'b00000 && hour < 'b01000) begin
+      cost = min_count * 1;
+   //8 am to 1 pm
+   end else if (hour >= 'b01000 && hour < 'b01101) begin
+      cost = min_count * 2;
+   //1 pm to 6 pm
+   end else if (hour >= 'b01101 && hour < 'b10010) begin
+      cost = min_count * 2;
+   //6 pm to 12 am
+   end else if (hour >= 'b10010 && hour < 'b11000) begin
+      cost = min_count * 1;
+   end
 
- end else if (location == 'b101) begin
-  //12 am to 8 am
-  if (hour >= 'b00000 && hour < 'b01000) begin
-   cost = min_count * 1.34;
-  //8 am to 1 pm
-  end else if (hour >= 'b01000 && hour < 'b01101) begin
-   cost = min_count * 1.56;
-  //1 pm to 6 pm
-  end else if (hour >= 'b01101 && hour < 'b10010) begin
-   cost = min_count * 1.78;
-  //6 pm to 12 am
-  end else if (hour >= 'b10010 && hour < 'b11000) begin
-     cost = min_count * 1.56;
-  end
+   end else if (location == 'b101) begin
+   //12 am to 8 am
+   if (hour >= 'b00000 && hour < 'b01000) begin
+      cost = min_count * 1;
+   //8 am to 1 pm
+   end else if (hour >= 'b01000 && hour < 'b01101) begin
+      cost = min_count * 2;
+   //1 pm to 6 pm
+   end else if (hour >= 'b01101 && hour < 'b10010) begin
+      cost = min_count * 2;
+   //6 pm to 12 am
+   end else if (hour >= 'b10010 && hour < 'b11000) begin
+      cost = min_count * 1;
+   end
 
- end else if (location == 'b110) begin
-  //12 am to 8 am
-  if (hour >= 'b00000 && hour < 'b01000) begin
-   cost = min_count * 1.34;
-  //8 am to 1 pm
-  end else if (hour >= 'b01000 && hour < 'b01101) begin
-   cost = min_count * 1.56;
-  //1 pm to 6 pm
-  end else if (hour >= 'b01101 && hour < 'b10010) begin
-   cost = min_count * 1.78;
-  //6 pm to 12 am
-  end else if (hour >= 'b10010 && hour < 'b11000) begin
-     cost = min_count * 1.56;
-  end
+   end else if (location == 'b110) begin
+   //12 am to 8 am
+   if (hour >= 'b00000 && hour < 'b01000) begin
+      cost = min_count * 1.34;
+   //8 am to 1 pm
+   end else if (hour >= 'b01000 && hour < 'b01101) begin
+      cost = min_count * 1.56;
+   //1 pm to 6 pm
+   end else if (hour >= 'b01101 && hour < 'b10010) begin
+      cost = min_count * 1.78;
+   //6 pm to 12 am
+   end else if (hour >= 'b10010 && hour < 'b11000) begin
+      cost = min_count * 1.56;
+   end
 
- end else if (location == 'b111) begin
-  //12 am to 8 am
-  if (hour >= 'b00000 && hour < 'b01000) begin
-   cost = min_count * 1.34;
-  //8 am to 1 pm
-  end else if (hour >= 'b01000 && hour < 'b01101) begin
-   cost = min_count * 1.56;
-  //1 pm to 6 pm
-  end else if (hour >= 'b01101 && hour < 'b10010) begin
-   cost = min_count * 1.78;
-  //6 pm to 12 am
-  end else if (hour >= 'b10010 && hour < 'b11000) begin
-     cost = min_count * 1.56;
-  end
- 
+   end else if (location == 'b111) begin
+   //12 am to 8 am
+   if (hour >= 'b00000 && hour < 'b01000) begin
+      cost = min_count * 1;
+   //8 am to 1 pm
+   end else if (hour >= 'b01000 && hour < 'b01101) begin
+      cost = min_count * 2;
+   //1 pm to 6 pm
+   end else if (hour >= 'b01101 && hour < 'b10010) begin
+      cost = min_count * 2;
+   //6 pm to 12 am
+   end else if (hour >= 'b10010 && hour < 'b11000) begin
+      cost = min_count * 1;
+   end
+   
+   end
  end
-end
-
-
 
 endmodule
 
